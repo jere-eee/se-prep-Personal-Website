ff20a12929e6394de4431b0ffce0e93074711a92
Akanle Jeremy <akanlejeremy@gmail.com>
2024-09-14 10:44:54 +0300

    adding comments and finishing touches

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ window.addEventListener("DOMContentLoaded", (e) => {
     const aboutSection = document.getElementById("about");
     const divs = [divOne, divTwo, divThree];
     
+    // Remove extraInfo div initially
     extraInfo.remove();
 
+    // Create function to show extraInfo div onclick
     function showDivs() {
         aboutSection.append(extraInfo);
         for (let i = 0; i < divs.length; i++) {
             divs[i].style.transition = "all ease-in-out 2s";
+            
+            // Have each div translated differently
             let translateValue = -40 + i * 10;
             divs[i].style.transform = `translateY(${translateValue}px)`;
         };    
     };
 
+    // Add event listener
      if (learnButton) {
         learnButton.addEventListener('click', showDivs)
     };
